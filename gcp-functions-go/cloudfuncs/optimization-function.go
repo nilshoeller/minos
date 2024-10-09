eaeef9b7379f9bb7d454b5050994d39ecfd11ff1
Nils <nilshoeller00@gmail.com>
2024-10-09 10:46:38 +0200

    put function in its own module

---
+++ gcp-functions-go/cloudfuncs/optimization-function.go
@@ -0,0 +1,66 @@
+package cloudfuncs
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/nilshoeller/bsc-thesis-implementation/gcp-functions-go/lib"
+	"github.com/nilshoeller/bsc-thesis-implementation/gcp-functions-go/model"
+)
+
+const maxRetries = 3
+
+// const url = "https://us-central1-bsc-thesis-implementation.cloudfunctions.net/optimizationFunctionGo"
+
+const url = "http://localhost:8080/"
+
+// Handler for the optimization function
+func OptimizationFunction(w http.ResponseWriter, r *http.Request) {
+	var req model.Request
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil && err.Error() != "EOF" {
+		fmt.Println("test")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.TaskID == "" {
+		req.TaskID = uuid.NewString()
+	}
+	if req.RetryCount < 0 {
+		req.RetryCount = 0
+	}
+	if req.TotalTimeOfExecution < 0 {
+		req.TotalTimeOfExecution = 0
+	}
+
+	// Immediate response to the client
+	response := model.Response{
+		Message: "Request received, processing...",
+		TaskID:  req.TaskID,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+
+	var benchmarkResult model.BenchmarkResult
+
+	// Perform benchmark
+	benchmarkResult.BenchmarkPassed, benchmarkResult.TimeOfExecution = lib.PerformBenchmark(100 * time.Millisecond)
+
+	if benchmarkResult.BenchmarkPassed {
+		fmt.Printf("Benchmark passed for taskId \"%s\" in time (%f).\n", req.TaskID, req.TotalTimeOfExecution)
+		return
+	}
+
+	if req.RetryCount < maxRetries {
+		req.RetryCount++
+		req.TotalTimeOfExecution += benchmarkResult.TimeOfExecution
+		lib.InvokeNew(url, req.TaskID, req.RetryCount, req.TotalTimeOfExecution, benchmarkResult.TimeOfExecution)
+		return
+	}
+	fmt.Printf("Max retries reached for taskId \"%s\".\n", req.TaskID)
+}
--- gcp-functions-go/main.go
+++ gcp-functions-go/main.go
@@ -1,77 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"time"
-
-	"github.com/nilshoeller/bsc-thesis-implementation/gcp-functions-go/lib"
-	"github.com/nilshoeller/bsc-thesis-implementation/gcp-functions-go/model"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	"github.com/google/uuid"
+	"github.com/nilshoeller/bsc-thesis-implementation/gcp-functions-go/cloudfuncs"
 )
 
-const maxRetries = 3
-
-const url = "https://us-central1-bsc-thesis-implementation.cloudfunctions.net/optimizationFunctionGo"
-
-// const url = "http://localhost:8080/"
-
 func init() {
-	functions.HTTP("OptimizationFunction", OptimizationFunction)
-}
-
-// Handler for the optimization function
-func OptimizationFunction(w http.ResponseWriter, r *http.Request) {
-	var req model.Request
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil && err.Error() != "EOF" {
-		fmt.Println("test")
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	if req.TaskID == "" {
-		req.TaskID = uuid.NewString()
-	}
-	if req.RetryCount < 0 {
-		req.RetryCount = 0
-	}
-	if req.TotalTimeOfExecution < 0 {
-		req.TotalTimeOfExecution = 0
-	}
-
-	// Immediate response to the client
-	response := model.Response{
-		Message: "Request received, processing...",
-		TaskID:  req.TaskID,
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
-
-	var benchmarkResult model.BenchmarkResult
-
-	// Perform benchmark
-	benchmarkResult.BenchmarkPassed, benchmarkResult.TimeOfExecution = lib.PerformBenchmark(100 * time.Millisecond)
-
-	if benchmarkResult.BenchmarkPassed {
-		fmt.Printf("Benchmark passed for taskId \"%s\" in time (%f).\n", req.TaskID, req.TotalTimeOfExecution)
-		return
-	}
-
-	if req.RetryCount < maxRetries {
-		req.RetryCount++
-		req.TotalTimeOfExecution += benchmarkResult.TimeOfExecution
-		lib.InvokeNew(url, req.TaskID, req.RetryCount, req.TotalTimeOfExecution, benchmarkResult.TimeOfExecution)
-		return
-	}
-	fmt.Printf("Max retries reached for taskId \"%s\".\n", req.TaskID)
+	functions.HTTP("OptimizationFunction", cloudfuncs.OptimizationFunction)
 }
 
 // Commands to start local testing
