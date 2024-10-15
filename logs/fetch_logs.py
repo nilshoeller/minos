6d2d487b5956f30dea208ec62a832edfeed98940
Nils <nilshoeller00@gmail.com>
2024-10-15 11:21:01 +0200

    both end markers have to be parsed to append

--- logs/fetch_logs.py
+++ logs/fetch_logs.py
@@ -1,6 +1,4 @@
 import json
-import csv
-import os
 from datetime import datetime
 
 import google.cloud.logging # type: ignore
@@ -8,6 +6,7 @@ from google.cloud.logging import ASCENDING # type: ignore
 # from google.cloud.logging import DESCENDING # type: ignore
 
 from log_utils import parse_logs
+from log_utils import csv_log_saver
 
 def get_cloud_function_logs(function_name, project_id, limit=10):
     # Create a client to access Cloud Logging
@@ -23,33 +22,8 @@ def get_cloud_function_logs(function_name, project_id, limit=10):
     # print("COUNT: ", len(list(entries)))
     print(f"Logs for Cloud Function: {function_name}\n")
 
-    return parse_logs.parse_between_markers(entries)
+    return parse_logs.parse_optimized_func(entries)
 
-def save_logs_to_csv(logs, folder_name='logs_archive'):
-    # Create a folder to store the CSV file if it doesn't exist
-    os.makedirs(folder_name, exist_ok=True)
-    
-    # Get the current date for the file name
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    file_name = f'cloud_function_logs_{current_date}.csv'  # Include the date in the file name
-
-
-    # Define the full path for the CSV file
-    csv_file_path = os.path.join(folder_name, file_name)
-
-    # Get the keys for the CSV header from the first log entry
-    if logs:
-        header = logs[0].keys()  # Assumes all log entries have the same structure
-
-        # Write logs to a CSV file
-        with open(csv_file_path, mode='w', newline='') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=header)
-            writer.writeheader()
-            writer.writerows(logs)
-
-        print(f"Logs saved to {csv_file_path}")
-    else:
-        print("No logs to save.")
 
 if __name__ == "__main__":
     # Replace with your Cloud Function name and Google Cloud project ID
@@ -57,10 +31,10 @@ if __name__ == "__main__":
     project_id = "bsc-thesis-implementation"
 
     # Fetch and print logs
-    logs = get_cloud_function_logs(function_name, project_id, limit=100)
+    logs = get_cloud_function_logs(function_name, project_id, 100)
 
     # # Print logs and return them as JSON
     # print(json.dumps(logs, indent=2))  # Pretty print JSON
 
     # Save logs to CSV
-    save_logs_to_csv(logs)
+    csv_log_saver.save_to_csv(function_name, logs)
--- logs/log_utils/parse_logs_unittest.py
+++ logs/log_utils/parse_logs_unittest.py
@@ -14,6 +14,7 @@ class TestLogExtractor(unittest.TestCase):
         # Prepare sample logs for testing
         logs = [
             # Sample log entries that mimic your log format
+            Log("DEBUG", "Function execution started", datetime.now()),
             Log(None, "=== START LOG ===", datetime.now()),
             Log(None, "Benchmark passed", datetime.now()),
             Log(None, "TaskId: 123", datetime.now()),
@@ -21,6 +22,7 @@ class TestLogExtractor(unittest.TestCase):
             Log(None, "=== END LOG ===", datetime.now()),
             Log("DEBUG", "Function execution took 8 ms, finished with status code: 200", datetime.now()),
 
+            Log("DEBUG", "Function execution started", datetime.now()),
             Log(None, "=== START LOG ===", datetime.now()),
             Log(None, "Benchmark passed", datetime.now()),
             Log(None, "TaskId: 321", datetime.now()),
@@ -28,41 +30,42 @@ class TestLogExtractor(unittest.TestCase):
             Log(None, "=== END LOG ===", datetime.now()),
             Log("DEBUG", "Function execution took 9 ms, finished with status code: 200", datetime.now()),
 
+            Log("DEBUG", "Function execution started", datetime.now()),
+            Log("DEBUG", "Function execution took 10 ms, finished with status code: 200", datetime.now()),
             Log(None, "=== START LOG ===", datetime.now()),
             Log(None, "Max retries reached", datetime.now()),
             Log(None, "TaskId: 456", datetime.now()),
             Log(None, "Retries: 3", datetime.now()),
             Log(None, "=== END LOG ===", datetime.now()),
-            Log("DEBUG", "Function execution took 10 ms, finished with status code: 200", datetime.now()),
         ]
         expected_output = [
             # Expected output for the sample logs
             {
-            "count": 0,
-            "timestamp": logs[5].timestamp.isoformat(),
+            "count": 1,
+            "timestamp": logs[6].timestamp.isoformat(),
             "log": "Benchmark passed",
             "taskid": "123",
             "retries": 1,
             "functionexectime": 8.0,
         },
         {
-            "count": 1,
-            "timestamp": logs[11].timestamp.isoformat(),
+            "count": 2,
+            "timestamp": logs[13].timestamp.isoformat(),
             "log": "Benchmark passed",
             "taskid": "321",
             "retries": 0,
             "functionexectime": 9.0,
         },
         {
-            "count": 2,
-            "timestamp": logs[17].timestamp.isoformat(),
+            "count": 3,
+            "timestamp": logs[15].timestamp.isoformat(),
             "log": "Max retries reached",
             "taskid": "456",
             "retries": 3,
             "functionexectime": 10.0,
         }
         ]
-        result = parse_logs.parse_between_markers(logs)
+        result = parse_logs.parse_optimized_func(logs)
         self.assertEqual(result, expected_output)
 
 if __name__ == "__main__":
